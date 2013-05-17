4b3688f7cdaea708084b10d5d07130b6a1886851
Frank Stratton <epanastasi@epanastasi.com>
2013-05-16 20:08:36 -0700

    beta version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from localtunnel import __version__
 
 setup(
     name='localtunnel',
-    version=__version__,
+    version='0.6.2beta',
     author='Jeff Lindsay',
     author_email='progrium@gmail.com',
     description='Magically expose a local port to the Internet',
